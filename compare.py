b541157c9eba9449c9e4be78959bfe3d82182462
hongleongyong <yonghongleong98@gmail.com>
2023-10-02 15:12:26 +0800

    update compare.py

--- compare.py
+++ compare.py
@@ -1,23 +1,31 @@
-# compare csv content by pandas
+"""
+This module compares two CSV files using pandas.
+"""
+
 import pandas as pd
 
-columns = ('id', 'name', 'age', 'address', 'phone')
-index_col = ('id')
+# Define constants
+COLUMNS = ['id', 'name', 'age', 'address', 'phone']
+INDEX_COL = 'id'
 
-df1 = pd.read_csv('file1.csv', encoding='utf-8', names=columns, index_col=index_col)
-df2 = pd.read_csv('file2.csv', encoding='utf-8', names=columns, index_col=index_col)
+def main():
+    """Main function to compare two CSV files."""
+    df1 = pd.read_csv('file1.csv', encoding='utf-8', names=COLUMNS, index_col=INDEX_COL)
+    df2 = pd.read_csv('file2.csv', encoding='utf-8', names=COLUMNS, index_col=INDEX_COL)
 
-df1.sort_index(inplace=True)
-df2.sort_index(inplace=True)
+    df1.sort_index(inplace=True)
+    df2.sort_index(inplace=True)
 
-sorted_df = df1.sort_values(by=['id'], ascending=True)
-print(sorted_df.head(5))
+    sorted_df = df1.sort_values(by=['id'], ascending=True)
+    print(sorted_df.head(5))
 
-print(len(df1.index))
+    print(len(df1.index))
 
-if len(df1.index) == len(df2.index):
-    if df1.equals(df2) == False:
-        # compare csv content
-        df_output = df1.compare(df2, result_names=('file1', 'file2'))
-        df_output.to_csv('output.csv', encoding='utf-8')
-        # print(df_output.info())
+    if len(df1.index) == len(df2.index):
+        if not df1.equals(df2):
+            # Compare CSV content
+            df_output = df1.compare(df2, result_names=('file1', 'file2'))
+            df_output.to_csv('output.csv', encoding='utf-8')
+
+if __name__ == "__main__":
+    main()
