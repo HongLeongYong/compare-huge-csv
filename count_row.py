c00afb9ec6b219a7826d8b44e14af904de889583
hongleongyong <yonghongleong98@gmail.com>
2023-10-06 10:54:25 +0800

    create unit_test file

--- count_row.py
+++ count_row.py
@@ -3,20 +3,21 @@ import time
 import global_variable as gv
 
 def csv_count_rows(file_name):
-    """Count the number of rows in a CSV file."""
+    """
+    Count the number of rows in a CSV file.
+    """
+    row_index = -1  # 初始化 row_index
+    with open(file_name, encoding='utf-8') as file_handle:
+        for index, _ in enumerate(file_handle):
+            row_index = index
+    return row_index + 1
+
+def __main__():
     start_time = time.time()
     print(start_time)
 
-    row_index = -1 # 初始化 row_index
-    with open(file_name, encoding='utf-8') as file_handle:
-        for row_index in enumerate(file_handle):
-            if row_index % 10_000_000 == 0:
-                middle_time = time.time()
-                print(f'{row_index} rows, use time: {middle_time - start_time} seconds')
+    print(f'before file has {csv_count_rows(gv.BEFORE_HUGE_CSV_FILE)} rows')
+    print(f'after file has {csv_count_rows(gv.AFTER_HUGE_CSV_FILE)} rows')
 
     end_time = time.time()
     print(f'end, use time: {end_time - start_time} seconds')
-    return row_index + 1
-
-print(f'before file has {csv_count_rows(gv.BEFORE_HUGE_CSV_FILE)} rows')
-print(f'after file has {csv_count_rows(gv.AFTER_HUGE_CSV_FILE)} rows')
---
+++ test_count_row.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import patch, mock_open
+import count_row  # 要測試的模組
+
+class TestCsvCountRows(unittest.TestCase):
+
+    @patch("builtins.open", new_callable=mock_open, read_data="data1\ndata2\ndata3\n")
+    def test_csv_count_rows(self, mock_file):
+        result = count_row.csv_count_rows("some_file.csv")
+        self.assertEqual(result, 3)
+
+def __main__():
+    unittest.main()
